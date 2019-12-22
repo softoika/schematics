74f8189985f9059054993d9673a0ef0627104889
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-12-22 10:41:05 +0900

    Specify a build result directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  "testPathIgnorePatterns": ["/node_modules/", "files"]
+  "testPathIgnorePatterns": ["/node_modules/", "files", "/dist/"]
 };
