1c4f44af0556335fb5c0f98237554157612d36a7
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2020-01-04 19:41:17 +0900

    Fix behavior about --project option

--- src/injectable/index.spec.ts
+++ src/injectable/index.spec.ts
@@ -7,12 +7,6 @@ import { Schema as ApplicationOptions } from '@schematics/angular/application/sc
 import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
 import { InjectableOptions } from './schema';
 
-const workspaceOptions: WorkspaceOptions = {
-  name: 'workspace',
-  newProjectRoot: 'projects',
-  version: '6.0.0'
-};
-
 const appOptions: ApplicationOptions = {
   name: 'bar',
   inlineStyle: false,
@@ -31,171 +25,242 @@ const collectionPath = path.join(__dirname, '../collection.json');
 const runner = new SchematicTestRunner('schematics', collectionPath);
 
 describe('Injectable Schematic', () => {
-  let appTree: UnitTestTree;
-
-  beforeEach(async () => {
-    appTree = await runner
-      .runExternalSchematicAsync(
-        '@schematics/angular',
-        'workspace',
-        workspaceOptions
-      )
-      .toPromise();
-    appTree = await runner
-      .runExternalSchematicAsync(
-        '@schematics/angular',
-        'application',
-        appOptions,
-        appTree
-      )
-      .toPromise();
+  describe('not in angular workspace', () => {
+    it('should create files in root', async () => {
+      const options = { name: defaultOptions.name, type: defaultOptions.type };
+      const tree = await runner
+        .runSchematicAsync('injectable', options)
+        .toPromise();
+      expect(tree.files).toContain('/user.repository.ts');
+    });
   });
 
-  it('should create injectable class files', async () => {
-    const tree = await runner
-      .runSchematicAsync('injectable', defaultOptions, appTree)
-      .toPromise();
-    expect(tree.files).toContain('/projects/bar/src/app/user.repository.ts');
-    expect(
-      tree.readContent('/projects/bar/src/app/user.repository.ts')
-    ).toEqual(
-      "import { Injectable } from '@angular/core';\n" +
-        '\n' +
-        '@Injectable({\n' +
-        "  providedIn: 'root'\n" +
-        '})\n' +
-        'export class UserRepository {\n' +
-        '\n' +
-        '  constructor() { }\n' +
-        '}\n\n'
-    );
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/user.repository.spec.ts'
-    );
-    expect(
-      tree.readContent('/projects/bar/src/app/user.repository.spec.ts')
-    ).toEqual(
-      "import { TestBed } from '@angular/core/testing';\n" +
-        '\n' +
-        "import { UserRepository } from './user.repository';\n" +
-        '\n' +
-        "describe('UserRepository', () => {\n" +
-        '  beforeEach(() => TestBed.configureTestingModule({}));\n' +
-        '\n' +
-        "  it('should be created', () => {\n" +
-        '    const repository: UserRepository = TestBed.get(UserRepository);\n' +
-        '    expect(repository).toBeTruthy();\n' +
-        '  });\n' +
-        '});\n' +
-        '\n'
-    );
-  });
-
-  it('should create files as service class without --type option', async () => {
-    const options: InjectableOptions = {
-      name: 'user',
-      project: defaultOptions.project
+  describe('multiple projects workspace', () => {
+    const workspaceOptions: WorkspaceOptions = {
+      name: 'workspace',
+      newProjectRoot: 'projects',
+      version: '6.0.0'
     };
-    const tree = await runner
-      .runSchematicAsync('injectable', options, appTree)
-      .toPromise();
-    expect(tree.files).toContain('/projects/bar/src/app/user.service.ts');
-    expect(tree.files).toContain('/projects/bar/src/app/user.service.spec.ts');
 
-    expect(tree.readContent('/projects/bar/src/app/user.service.ts')).toContain(
-      'UserService'
-    );
+    let appTree: UnitTestTree;
 
-    const specContent = tree.readContent(
-      '/projects/bar/src/app/user.service.spec.ts'
-    );
-    expect(specContent).toContain(
-      "import { UserService } from './user.service'"
-    );
-    expect(specContent).toContain(
-      'const service: UserService = TestBed.get(UserService);'
-    );
+    beforeEach(async () => {
+      appTree = await runner
+        .runExternalSchematicAsync(
+          '@schematics/angular',
+          'workspace',
+          workspaceOptions
+        )
+        .toPromise();
+      appTree = await runner
+        .runExternalSchematicAsync(
+          '@schematics/angular',
+          'application',
+          appOptions,
+          appTree
+        )
+        .toPromise();
+    });
+
+    it('should create injectable class files', async () => {
+      const tree = await runner
+        .runSchematicAsync('injectable', defaultOptions, appTree)
+        .toPromise();
+      expect(tree.files).toContain('/projects/bar/src/app/user.repository.ts');
+      expect(
+        tree.readContent('/projects/bar/src/app/user.repository.ts')
+      ).toEqual(
+        "import { Injectable } from '@angular/core';\n" +
+          '\n' +
+          '@Injectable({\n' +
+          "  providedIn: 'root'\n" +
+          '})\n' +
+          'export class UserRepository {\n' +
+          '\n' +
+          '  constructor() { }\n' +
+          '}\n\n'
+      );
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/user.repository.spec.ts'
+      );
+      expect(
+        tree.readContent('/projects/bar/src/app/user.repository.spec.ts')
+      ).toEqual(
+        "import { TestBed } from '@angular/core/testing';\n" +
+          '\n' +
+          "import { UserRepository } from './user.repository';\n" +
+          '\n' +
+          "describe('UserRepository', () => {\n" +
+          '  beforeEach(() => TestBed.configureTestingModule({}));\n' +
+          '\n' +
+          "  it('should be created', () => {\n" +
+          '    const repository: UserRepository = TestBed.get(UserRepository);\n' +
+          '    expect(repository).toBeTruthy();\n' +
+          '  });\n' +
+          '});\n' +
+          '\n'
+      );
+    });
+
+    it('should create files as service class without --type option', async () => {
+      const options: InjectableOptions = {
+        name: 'user',
+        project: defaultOptions.project
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain('/projects/bar/src/app/user.service.ts');
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/user.service.spec.ts'
+      );
+
+      expect(
+        tree.readContent('/projects/bar/src/app/user.service.ts')
+      ).toContain('UserService');
+
+      const specContent = tree.readContent(
+        '/projects/bar/src/app/user.service.spec.ts'
+      );
+      expect(specContent).toContain(
+        "import { UserService } from './user.service'"
+      );
+      expect(specContent).toContain(
+        'const service: UserService = TestBed.get(UserService);'
+      );
+    });
+
+    it('should create files in the path if name is provided with path', async () => {
+      const options: InjectableOptions = {
+        ...defaultOptions,
+        name: 'foo/user'
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/foo/user.repository.ts'
+      );
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/foo/user.repository.spec.ts'
+      );
+      expect(
+        tree.readContent('/projects/bar/src/app/foo/user.repository.ts')
+      ).toContain('export class UserRepository {');
+      expect(
+        tree.readContent('/projects/bar/src/app/foo/user.repository.spec.ts')
+      ).toContain("import { UserRepository } from './user.repository'");
+    });
+
+    it('should create files in the named directory if --flat optin is false', async () => {
+      const options: InjectableOptions = {
+        ...defaultOptions,
+        flat: false
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/user/user.repository.ts'
+      );
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/user/user.repository.spec.ts'
+      );
+    });
+
+    it(`should create files in '/path/the-named-directory' if path and --flat false are provided`, async () => {
+      const options: InjectableOptions = {
+        ...defaultOptions,
+        name: 'path/user',
+        flat: false
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/path/user/user.repository.ts'
+      );
+      expect(tree.files).toContain(
+        '/projects/bar/src/app/path/user/user.repository.spec.ts'
+      );
+    });
+
+    it('should create files in the custom project root', async () => {
+      const config = JSON.parse(appTree.readContent('/angular.json'));
+      expect(config?.projects?.bar).toBeDefined();
+      config.projects.bar.sourceRoot = 'projects/bar/custom';
+      appTree.overwrite('/angular.json', JSON.stringify(config, null, 2));
+      appTree = await runner
+        .runSchematicAsync('injectable', defaultOptions, appTree)
+        .toPromise();
+      expect(appTree.files).toContain(
+        '/projects/bar/custom/app/user.repository.ts'
+      );
+    });
+
+    it('should throw an error if --project is not passed', async () => {
+      const options = {
+        name: defaultOptions.name,
+        type: defaultOptions.type
+      };
+      await expect(
+        runner.runSchematicAsync('injectable', options, appTree).toPromise()
+      ).rejects.toThrowError('Specified project does not exist.');
+    });
+
+    it('should not create a test file if --skipTests is true', async () => {
+      const options: InjectableOptions = {
+        ...defaultOptions,
+        skipTests: true
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain('/projects/bar/src/app/user.repository.ts');
+      expect(tree.files).not.toContain(
+        '/projects/bar/src/app/user.repository.spec.ts'
+      );
+    });
   });
 
-  it('should create files in the path if name is provided with path', async () => {
-    const options: InjectableOptions = {
-      ...defaultOptions,
-      name: 'foo/user'
+  describe('single project workspace', () => {
+    const workspaceOptions: WorkspaceOptions = {
+      name: 'workspace',
+      version: '6.0.0'
     };
-    const tree = await runner
-      .runSchematicAsync('injectable', options, appTree)
-      .toPromise();
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/foo/user.repository.ts'
-    );
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/foo/user.repository.spec.ts'
-    );
-    expect(
-      tree.readContent('/projects/bar/src/app/foo/user.repository.ts')
-    ).toContain('export class UserRepository {');
-    expect(
-      tree.readContent('/projects/bar/src/app/foo/user.repository.spec.ts')
-    ).toContain("import { UserRepository } from './user.repository'");
-  });
 
-  it('should create files in the named directory if --flat optin is false', async () => {
-    const options: InjectableOptions = {
-      ...defaultOptions,
-      flat: false
-    };
-    const tree = await runner
-      .runSchematicAsync('injectable', options, appTree)
-      .toPromise();
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/user/user.repository.ts'
-    );
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/user/user.repository.spec.ts'
-    );
-  });
+    let appTree: UnitTestTree;
 
-  it(`should create files in '/path/the-named-directory' if path and --flat false are provided`, async () => {
-    const options: InjectableOptions = {
-      ...defaultOptions,
-      name: 'path/user',
-      flat: false
-    };
-    const tree = await runner
-      .runSchematicAsync('injectable', options, appTree)
-      .toPromise();
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/path/user/user.repository.ts'
-    );
-    expect(tree.files).toContain(
-      '/projects/bar/src/app/path/user/user.repository.spec.ts'
-    );
-  });
+    beforeEach(async () => {
+      appTree = await runner
+        .runExternalSchematicAsync(
+          '@schematics/angular',
+          'workspace',
+          workspaceOptions
+        )
+        .toPromise();
+      appTree = await runner
+        .runExternalSchematicAsync(
+          '@schematics/angular',
+          'application',
+          appOptions,
+          appTree
+        )
+        .toPromise();
+    });
 
-  it('should create files in the custom project root', async () => {
-    const config = JSON.parse(appTree.readContent('/angular.json'));
-    expect(config?.projects?.bar).toBeDefined();
-    config.projects.bar.sourceRoot = 'projects/bar/custom';
-    appTree.overwrite('/angular.json', JSON.stringify(config, null, 2));
-    appTree = await runner
-      .runSchematicAsync('injectable', defaultOptions, appTree)
-      .toPromise();
-    expect(appTree.files).toContain(
-      '/projects/bar/custom/app/user.repository.ts'
-    );
-  });
-
-  it('should not create a test file if --skipTests is true', async () => {
-    const options: InjectableOptions = {
-      ...defaultOptions,
-      skipTests: true
-    };
-    const tree = await runner
-      .runSchematicAsync('injectable', options, appTree)
-      .toPromise();
-    expect(tree.files).toContain('/projects/bar/src/app/user.repository.ts');
-    expect(tree.files).not.toContain(
-      '/projects/bar/src/app/user.repository.spec.ts'
-    );
+    it('should create files in default project path', async () => {
+      const config = JSON.parse(appTree.readContent('/angular.json'));
+      const options = {
+        name: defaultOptions.name,
+        type: defaultOptions.type,
+        // The defaultProject is passed implicitly to --project by Angular CLI if in a single project workspace.
+        project: config.defaultProject
+      };
+      const tree = await runner
+        .runSchematicAsync('injectable', options, appTree)
+        .toPromise();
+      expect(tree.files).toContain('/bar/src/app/user.repository.ts');
+    });
   });
 });
--- src/injectable/index.ts
+++ src/injectable/index.ts
@@ -12,17 +12,18 @@ import {
   chain
 } from '@angular-devkit/schematics';
 import { strings } from '@angular-devkit/core';
+import { WorkspaceDefinition } from '@angular-devkit/core/src/workspace/definitions';
 import { parseName } from '@schematics/angular/utility/parse-name';
-import { createDefaultPath } from '@schematics/angular/utility/workspace';
+import {
+  getWorkspace,
+  buildDefaultPath
+} from '@schematics/angular/utility/workspace';
 import { applyLintFix } from '@schematics/angular/utility/lint-fix';
 import { InjectableOptions } from './schema';
 
 export default function(options: InjectableOptions): Rule {
   return async (tree: Tree, _context: SchematicContext) => {
-    let appliedOptions = options;
-    if (options.project) {
-      appliedOptions = await applyDefaultPath(options, tree);
-    }
+    let appliedOptions = await applyDefaultPath(options, tree);
     appliedOptions = applyNameParsing(appliedOptions);
 
     const templates = apply(url('./files'), [
@@ -48,17 +49,29 @@ async function applyDefaultPath(
 ): Promise<InjectableOptions> {
   const { path, project, ...remainingOptions } = options;
 
-  let defaultPath: string | undefined;
-  if (path === undefined && typeof project === 'string') {
-    defaultPath = await createDefaultPath(tree, project);
+  let workspace: WorkspaceDefinition | undefined;
+  try {
+    workspace = await getWorkspace(tree);
+  } catch (error) {
+    // Make it work even if not in angular workspace
+    workspace = undefined;
   }
 
-  const newOptions = {
+  let defaultPath: string | undefined;
+  if (workspace) {
+    const projectDefinition = workspace.projects.get(project as string);
+    if (projectDefinition) {
+      defaultPath = buildDefaultPath(projectDefinition);
+    } else {
+      throw new Error('Specified project does not exist.');
+    }
+  }
+
+  return {
     path: defaultPath ?? path,
     project,
     ...remainingOptions
   };
-  return new Promise<InjectableOptions>(resolve => resolve(newOptions));
 }
 
 function applyNameParsing(options: InjectableOptions): InjectableOptions {
